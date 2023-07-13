7eae6f99805cdbf76bc1d1ec0ed1311b0c17a609
FatimaALzahrani <12fatimah.15@gmail.com>
2023-07-14 01:17:05 +0300

    Initial commit

---
+++ app/src/main/java/com/plant/community/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.plant.community.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.plant.community.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
